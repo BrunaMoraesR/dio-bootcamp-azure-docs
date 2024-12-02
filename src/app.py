209e1b1da9fb61083abc0161c10b11d15473816d
Bruna de Moraes <60974864+BrunaMoraesR@users.noreply.github.com>
2024-12-01 21:15:29 -0300

    Azure integration and viewing data results

--- src/app.py
+++ src/app.py
@@ -1,5 +1,34 @@
 import streamlit as st
+from services.blob_service import upload_blob
+from services.credit_card_service import detect_credit_card_info
 
 def configure_interface():
     st.title("Upload de arquivos DIO - Desafio 1 - Azure - Fake Docs")
-    uploaded_file = st.file_uploaded("Escolha um arquivo", type=["png", "jpg", "jpeg"])
+    uploaded_file = st.file_uploader("Escolha um arquivo", type=["png", "jpg", "jpeg"])
+    
+    if uploaded_file is not None:
+        fileName = uploaded_file.name
+        blob_url = upload_blob(uploaded_file, fileName)
+        if blob_url is not None:
+            st.write(f"Arquivo {fileName} enviado com sucesso para o Azure Blob Storage")
+            credit_card_info = detect_credit_card_info(blob_url)
+            show_image_and_validation(blob_url, credit_card_info)
+        else:
+            st.write(f"Erro ao enviar o arquivo {fileName} para o Azure Blob Storage")
+
+def show_image_and_validation(blob_url, credit_card_info):
+    st.image(blob_url, caption="Imagem enviada", use_container_width=True)
+    st.write("Resultado da validação: ")
+    st.write("Informações do cartão de crédito encontradas: ")
+    st.write(credit_card_info)
+    if credit_card_info and credit_card_info['card_name']:
+        st.markdown("<h1 style='color: green;'>Cartão Válido</h1>", unsafe_allow_html=True)
+        st.write(f"Nome do titular: {credit_card_info['card_name']}")
+        st.write(f"Banco emissor: {credit_card_info['bank_name']}")
+        st.write(f"Data de validade: {credit_card_info['expiry_date']}")
+    else:
+        st.markdown(f"<h1 style='color: red;'>Cartão Inválido</h1>", unsafe_allow_html=True)
+        st.write("Este não é um número de cartão de crédito válido")
+
+if __name__ == "__main__":
+    configure_interface()
